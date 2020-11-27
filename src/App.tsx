dfbf2cd086d7b582fb12949af843b8437cc96cf0
Denis Toledo <contato@denistoledo.com.br>
2020-11-27 01:06:37 -0300

    created layout base with styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import Layout from './layouts/Layout';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Routes />
+      <Layout>
+        <Routes />
+      </Layout>
     </BrowserRouter>
   );
 };
---
+++ src/layouts/Layout/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Layout: React.FC = ({ children }) => (
+  <div className="w-screen min-h-screen flex flex-col items-center bg-gray-100 pt-20">
+    <nav className="fixed top-0 left-0 w-full flex flex-col items-center bg-gray-700 text-gray-50 p-4">
+      <header className="w-full max-w-screen-xl px-4">
+        <h1 className="flex items-center antialiased tracking-wide font-bold">
+          <span className="flex items-center justify-center w-10 h-10 bg-blue-200 rounded text-gray-800 mr-2">
+            CTA
+          </span>
+          <div>
+            Train Tracker
+            <span className="block font-normal text-xs italic">
+              Get estimated arrival times
+            </span>
+          </div>
+        </h1>
+      </header>
+    </nav>
+
+    <main className="flex-1 w-full max-w-screen-xl p-4">{children}</main>
+
+    <footer className="w-screen bg-gray-700 text-gray-50 p-2 text-center">
+      <small>©2020 Chicago Transit Authority</small>
+    </footer>
+  </div>
+);
+
+export default Layout;
