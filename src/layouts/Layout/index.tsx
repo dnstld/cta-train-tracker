daecd66d29a19581d02b26dfafa6182c93048bfe
Denis Toledo <contato@denistoledo.com.br>
2020-11-27 13:38:18 -0300

    added stations ui

--- src/layouts/Layout/index.tsx
+++ src/layouts/Layout/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Layout: React.FC = ({ children }) => (
   <div className="w-screen min-h-screen flex flex-col items-center bg-gray-100 pt-20">
-    <nav className="fixed top-0 left-0 w-full flex flex-col items-center bg-gray-700 text-gray-50 p-4">
-      <header className="w-full max-w-screen-xl px-4">
+    <nav className="fixed top-0 left-0 z-10 w-full flex bg-gray-700 text-gray-50 p-4">
+      <header className="">
         <h1 className="flex items-center antialiased tracking-wide font-bold">
           <span className="flex items-center justify-center w-10 h-10 bg-blue-200 rounded text-gray-800 mr-2">
             CTA
@@ -18,7 +18,7 @@ const Layout: React.FC = ({ children }) => (
       </header>
     </nav>
 
-    <main className="flex-1 w-full max-w-screen-xl p-4">{children}</main>
+    <main className="flex-1 w-full max-w-screen-2xl p-4">{children}</main>
 
     <footer className="w-screen bg-gray-700 text-gray-50 p-2 text-center">
       <small>©2020 Chicago Transit Authority</small>
--- src/pages/Stations/index.tsx
+++ src/pages/Stations/index.tsx
@@ -43,26 +43,59 @@ const Stations: React.FC = () => {
       });
   }, [setStations]);
 
+  function headingTo(h: string): void {
+    const headingMap: { [unit: string]: any } = {
+      N: 'North',
+      E: 'East',
+      S: 'South',
+      W: 'West',
+    };
+
+    return headingMap[h];
+  }
+
   return (
     <>
-      {stations.map((station: CTAStations) => (
-        <Link
-          key={station.STATION_NAME}
-          to={`/details/${station.STATION_NAME}`}
-        >
-          {station.STATION_NAME}
-          {station.STOPS.map((stop: CTAStationStops) => (
-            <div key={stop.STOP_ID}>
-              <p>{stop.STOP_ID}</p>
-              <p>{stop.DIRECTION}</p>
-              <p>{stop.STOP_NAME}</p>
-              <p>{stop.STATION_DESCRIPTIVE_NAME}</p>
-              <p>{stop.MAP_ID}</p>
-              <p>{stop.LOCATION}</p>
+      <header className="p-4">
+        <h1 className="text-4xl antialiased font-black tracking-wide text-gray-800">
+          Stations
+        </h1>
+      </header>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 lg:gap-6 xl:gap-8 p-4">
+        {stations.map((station: CTAStations) => (
+          <Link
+            key={station.STATION_NAME}
+            to={`/details/${station.STATION_NAME}`}
+            className="transition shadow-sm hover:shadow-lg transform scale-100 hover:scale-105 focus:scale-105 bg-gray-50 hover:bg-white focus:outline-none focus:bg-white rounded-t-2xl select-none p-4"
+          >
+            <header className="text-xl font-bold text-gray-800 mb-4">
+              <h2>{station.STATION_NAME}</h2>
+            </header>
+
+            <div className="grid gap-4">
+              <h3 className="text-base font-normal text-gray-500">Stops:</h3>
+
+              {station.STOPS.map((stop: CTAStationStops) => (
+                <div key={stop.STOP_ID} className="p-2 bg-gray-100 rounded">
+                  <small className="mb-4 text-gray-400">
+                    Heading {headingTo(stop.DIRECTION)}
+                  </small>
+                  <hr />
+                  <div className="flex flex-col mt-2">
+                    <span className="font-bold text-gray-600">
+                      {stop.STOP_NAME}
+                    </span>
+                    <span className="text-sm italic text-gray-400">
+                      - {stop.STATION_DESCRIPTIVE_NAME}
+                    </span>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
-        </Link>
-      ))}
+          </Link>
+        ))}
+      </div>
     </>
   );
 };
